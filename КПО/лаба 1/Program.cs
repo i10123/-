791b92a47d704285789d58b1dcd6575774b880dd
i10123 <lyckguddd@gmail.com>
2025-09-21 17:56:14 +0300

    Update Program.cs

--- КПО/лаба 1/Program.cs
+++ КПО/лаба 1/Program.cs
@@ -1,24 +1,17 @@
 using System;
 using System.IO;
 
-namespace КПО_лаба_1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            string avtor = "Karakulko Denis";
-
-            Console.WriteLine("Выберите номер набора данных:");
-            Console.WriteLine("0. sequences.0.txt / commands.0.txt");
-            Console.WriteLine("1. sequences.1.txt / commands.1.txt");
-            Console.WriteLine("2. sequences.2.txt / commands.2.txt");
-            Console.Write("Введите номер (0–2): ");
+namespace КПО_лаба_1{
+    internal class Program{
+        static void Main(string[] args){
+            Console.WriteLine("0. X = 0");
+            Console.WriteLine("1. X = 1");
+            Console.WriteLine("2. X = 2");
+            Console.Write("Выберите номер набора данных (sequences.X & commands.X): ");
 
             string N = Console.ReadLine();
-            while (N != "0" && N != "1" && N != "2")
-            {
-                Console.Write("Неверный ввод! Введите 0, 1 или 2: ");
+            while (N != "0" && N != "1" && N != "2"){
+                Console.Write("Неверный ввод! Повторите: ");
                 N = Console.ReadLine();
             }
 
@@ -26,22 +19,15 @@ namespace КПО_лаба_1
             string inputCommands = $"commands.{N}.txt";
             string outputGenedata = $"genedata.{N}.txt";
 
-            // наличие входных файлов
-            if (!File.Exists(inputSequences)){
-                Console.WriteLine($"Файл {inputSequences} не найден. Операция отменена.");
+            if (!File.Exists(inputSequences) || !File.Exists(inputCommands)){
+                Console.WriteLine("Файл не найден!");
                 return;
             }
 
-            if (!File.Exists(inputCommands)){
-                Console.WriteLine($"Файл {inputCommands} не найден. Операция отменена.");
-                return;
-            }
-
-             var processor = new GeneticCode(avtor);
-             processor.LoadSequences(inputSequences);
-             processor.ProcessCommands(inputCommands, outputGenedata);
-
-             Console.WriteLine($"Результаты записаны в {outputGenedata}");
+            var proc = new GeneticCode();
+            proc.LoadSequences(inputSequences);
+            proc.Processing_Commands_and_Writing_Result(inputCommands, outputGenedata);
+            Console.WriteLine("Результаты записаны!");
         }
     }
 }
