e760d980ca89e4a55df071c96cef79a14831910a
Denis <lyckguddd@gmail.com>
2025-09-26 10:51:32 +0300

    Update Program.cs

--- КПО/лаба 1/Program.cs
+++ КПО/лаба 1/Program.cs
@@ -1,32 +1,48 @@
 using System;
 using System.IO;
+using System.Text;
 
-namespace КПО_лаба_1{
-    internal class Program{
-        static void Main(string[] args){
+namespace КПО_лаба_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
             Console.WriteLine("0. X = 0");
             Console.WriteLine("1. X = 1");
             Console.WriteLine("2. X = 2");
             Console.Write("Выберите номер набора данных (sequences.X & commands.X): ");
 
             string N = Console.ReadLine();
-            while (N != "0" && N != "1" && N != "2"){
-                Console.Write("Неверный ввод! Повторите: ");
-                N = Console.ReadLine();
+            while (true)
+            {
+                switch (N)
+                {
+                    case "0":
+                    case "1":
+                    case "2":
+                        break;
+                    default:
+                        Console.Write("Неверный ввод! Повторите: ");
+                        N = Console.ReadLine();
+                        continue;
+                }
+                break;
             }
 
-            string inputSequences = $"sequences.{N}.txt";
-            string inputCommands = $"commands.{N}.txt";
-            string outputGenedata = $"genedata.{N}.txt";
+            StringBuilder inputSequences = new StringBuilder("sequences.").Append(N).Append(".txt");
+            StringBuilder inputCommands = new StringBuilder("commands.").Append(N).Append(".txt");
+            StringBuilder outputGenedata = new StringBuilder("genedata.").Append(N).Append(".txt");
 
-            if (!File.Exists(inputSequences) || !File.Exists(inputCommands)){
+            if (!File.Exists(inputSequences.ToString()) || !File.Exists(inputCommands.ToString()))
+            {
                 Console.WriteLine("Файл не найден!");
                 return;
             }
 
             var proc = new GeneticCode();
-            proc.LoadSequences(inputSequences);
-            proc.Processing_Commands_and_Writing_Result(inputCommands, outputGenedata);
+            proc.LoadSequences(inputSequences.ToString());
+            proc.Processing_Commands_and_Writing_Result(inputCommands.ToString(), outputGenedata.ToString());
             Console.WriteLine("Результаты записаны!");
         }
     }
