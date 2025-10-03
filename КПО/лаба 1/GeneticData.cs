5c67d5160c56f6002887ffa007c116190dc25868
Denis <lyckguddd@gmail.com>
2025-10-03 17:22:39 +0300

    Create GeneticData.cs

---
+++ КПО/лаба 1/GeneticData.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace КПО_лаба_1
+{
+    public struct GeneticData
+    {
+        public string protein;
+        public string organism;
+        public string amino_acids;
+    }
+}
